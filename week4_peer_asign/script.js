9508723a0578b3f5908d862b70fa6a3f1f82c4e7
mquan97 <minhquanhoang97@gmail.com>
2022-01-05 00:13:17 +0200

    script.js fix

--- week4_peer_asign/script.js
+++ week4_peer_asign/script.js
@@ -4,7 +4,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 
 
 for (var i = 0; i < names.length; i++)  {
-  var firstLetter = names[i].charAt(0).toLowerCase;
+  var firstLetter = names[i].charAt(0).toLowerCase();
   if (firstLetter === 'j')  {
     byeJ.speak(names[i]);
   } else {
